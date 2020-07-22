a851e47b8cda076d891012c736759bba31e98299
younoknowme <31772419+younoknowme@users.noreply.github.com>
2020-07-22 10:31:54 +0530

    Update Divide.java

--- TASK_2/src/main/java/Divide.java
+++ TASK_2/src/main/java/Divide.java
@@ -1,5 +1,10 @@
 public class Divide implements Calculator {
     public void calculate(long a, long b){
-        System.out.println("The Division of "+a+" by "+b+" is = "+(a/b));
+        if(b==0) {
+            System.out.println("Division by zero is not Supported");
+        }
+        else{
+            System.out.println("The Division of "+a+" by "+b+" is = "+(a/b));
+        }
     }
 }
